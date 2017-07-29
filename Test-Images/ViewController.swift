bded0f882d1f202c1e5ccb1f1e0fb0ee53ca7678
Alejandro Sanchez <asanchez@cognitiva.la>
2017-07-29 11:54:56 -0600

    Initial Commit

---
+++ Test-Images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test-Images
+//
+//  Created by Alejandro Sanchez on 29/7/17.
+//  Copyright © 2017 Alejandro Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
