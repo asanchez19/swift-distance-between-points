729467ae35a6c12587587af2ff338e130b81b006
Davide <davide@MacBook-Pro-DVP.local>
2017-08-01 15:50:45 -0600

    Agrega la funcionalidad de los puntos y la linea, ajustes menores en diseño

--- Test-Images/ViewController.swift
+++ Test-Images/ViewController.swift
@@ -12,22 +12,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
 
     @IBOutlet weak var p1Info: UILabel!
     @IBOutlet weak var p2Info: UILabel!
-    @IBOutlet weak var distanciaInfo: UILabel!
-    var esperando = true
-    var p1 : CGPoint? = nil
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    
+	@IBOutlet weak var distanciaInfo: UILabel!
+	@IBOutlet weak var imageView: UIImageView!
+	var primero = true
+	var p1 : CGPoint? = nil
+	var circulo1 = CAShapeLayer()
+	var circulo2 = CAShapeLayer()
+	var linea = CAShapeLayer()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.distanciaInfo.text = " "
         self.p1Info.text = " "
-        self.p2Info.text = " "
-        self.esperando = false
-        
+		self.p2Info.text = " "
+		self.primero = false
+		
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapAction))
         //  let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector(("tapAction")))
         
@@ -42,24 +43,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     
     func tapAction(sender: UITapGestureRecognizer) {
         
-        let aux = sender.location(in: self.imageView) // Change to whatever view you want the point for
-        
+        var aux = sender.location(in: self.imageView) // Change to whatever view you want the point for
+		
+		aux = correccion(punto: aux)
+		
         print(aux)
-        if esperando {
-            p2Info.text = "\(aux.x)  ,   \(aux.y)"
-            self.esperando = false
-            self.distanciaInfo.text = self.calcularDistancia(p2: aux)
-        } else {
-            self.p1Info.text = "\(aux.x)  ,   \(aux.y)"
-            self.p2Info.text = " "
-            self.distanciaInfo.text = " "
-            self.p1 = aux
-            self.esperando = true
-        }
-        
+		if primero {
+			p2Info.text = "\(aux.x)  ,   \(aux.y)"
+			self.primero = false
+			self.distanciaInfo.text = self.calcularDistancia(p2: aux)
+		} else {
+			self.p1Info.text = "\(aux.x)  ,   \(aux.y)"
+			self.p2Info.text = " "
+			self.distanciaInfo.text = " "
+			self.p1 = aux
+			self.primero = true
+		}
+		dibujaPunto(punto: aux)
+		
     }
-    
-    
+	
     func calcularDistancia(p2: CGPoint) -> String {
         let xDist = (self.p1?.x)! - p2.x
         let yDist = (self.p1?.y)! - p2.y
@@ -67,8 +70,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         
         return "\(distance)"
     }
-    
-    
+	
     @IBAction func cameraAction(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera){
             let imagePicker = UIImagePickerController()
@@ -86,12 +88,60 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		
         if  let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
-        imageView.image  = image
-        }else {
-        print("Error..")
+			imageView.image  = image
+        } else {
+			print("Error..")
         }
         self.dismiss(animated: false, completion: nil)
     }
+	
+	func correccion(punto: CGPoint) -> CGPoint {
+		var aux = punto
+		aux.x = punto.x
+		aux.y = punto.y + 88
+		
+		return aux
+	}
+	
+	func dibujaPunto(punto: CGPoint) {
+		let circlePath = UIBezierPath(arcCenter: CGPoint(x: punto.x, y: punto.y), radius: CGFloat(1), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+		
+		if primero {
+			configCirculo(forma: circulo1, caracteristicas: circlePath)
+			view.layer.addSublayer(circulo1)
+		} else {
+			configCirculo(forma: circulo2, caracteristicas: circlePath)
+			view.layer.addSublayer(circulo2)
+			dibujaLinea(p2: punto)
+		}
+	}
+	
+	func configCirculo(forma: CAShapeLayer, caracteristicas: UIBezierPath) {
+		forma.path = caracteristicas.cgPath
+		forma.fillColor = UIColor.clear.cgColor
+		forma.strokeColor = UIColor.red.cgColor
+		forma.lineWidth = 1.0	// Grosor de la linea
+	}
+	
+	func dibujaLinea(p2:CGPoint) {
+		
+		// borra la linea si ya existe
+		let linePath = UIBezierPath()
+		self.linea.path = linePath.cgPath
+		
+		// asigna la posición a la nueva linea
+		self.linea = CAShapeLayer()
+		linePath.move(to: p1!)
+		linePath.addLine(to: p2)
+		self.linea.path = linePath.cgPath
+		self.linea.strokeColor = UIColor.red.cgColor
+		self.linea.lineWidth = 1
+		self.linea.lineJoin = kCALineJoinRound
+		self.view.layer.addSublayer(linea)
+	}
+	
+	
 }
 
